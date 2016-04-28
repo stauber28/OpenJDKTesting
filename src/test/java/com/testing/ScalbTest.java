1a2af86821383c5b24b434bac465c42204b3f44c
kyserpa <pakkyser@yahoo.com>
2016-04-28 04:03:13 -0400

    fix ScalbTest

--- src/test/java/com/testing/ScalbTest.java
+++ src/test/java/com/testing/ScalbTest.java
@@ -27,43 +27,43 @@ public class ScalbTest extends TestCase{
 
     public void testCase1()
     {
-        double actual = Math.scalb(1,-3);
-        double expected = .0125;
+        double actual = Math.scalb(1.0,-3);
+        double expected = .125;
         Assert.assertEquals(actual, expected);
     }
 
     public void testCase2()
     {
-        double actual = Math.scalb(2,2);
-        double expected = 4;
+        double actual = Math.scalb(2.0,2);
+        double expected = 8;
         Assert.assertEquals(actual, expected);
     }
 
     public void testCase3()
     {
-        double actual = Math.scalb(1,-512);
-        double expected = 1;
+        double actual = Math.scalb(1.0,-512);
+        double expected = 7.458340731200207E-155;
         Assert.assertEquals(actual, expected);
     }
     
     public void testCase4()
     {
-        double actual = Math.scalb(1,-1024);
-        double expected = .0125;
+        double actual = Math.scalb(1.0,-1024);
+        double expected = 5.562684646268003E-309;
         Assert.assertEquals(actual, expected);
     }
     
     public void testCase5()
     {
-        double actual = Math.scalb(1,512);
-        double expected = 1;
+        double actual = Math.scalb(1.0,512);
+        double expected = 1.3407807929942597E154;
         Assert.assertEquals(actual, expected);
     }
     
     public void testCase6()
     {
         double actual = Math.scalb(1,1024);
-        double expected = 1;
+        double expected = Double.POSITIVE_INFINITY;
         Assert.assertEquals(actual, expected);
     }
 
